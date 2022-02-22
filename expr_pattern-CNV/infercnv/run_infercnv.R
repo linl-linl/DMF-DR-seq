fe0f5d1b5089964aeaf24fab8f04763440346095
linl-linl <98104097+linl-linl@users.noreply.github.com>
2022-02-22 10:59:31 +0800

    Delete expr_pattern-CNV directory

--- expr_pattern-CNV/infercnv/run_infercnv.R
+++
@@ -1,4 +0,0 @@
-library(infercnv)
-infercnv_obj = CreateInfercnvObject(raw_counts_matrix='./DGE_count.txt', annotations_file='./annotation_count.txt', delim="\t", gene_order_file="./gene_order.txt", ref_group_names=c('normal'))
-infercnv_obj = infercnv::run(infercnv_obj, cutoff=1, out_dir=".", cluster_by_groups=T, denoise=T, HMM=F, output_format = "pdf")
-
